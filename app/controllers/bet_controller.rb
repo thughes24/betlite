d8b19497dbd4f27e6ab66752e7a4fa49b71bddc5
Tom Hughes <thomas.peter.hughes@gmail.com>
2017-06-30 11:20:11 +0930

    addded graph to UI

--- app/controllers/bet_controller.rb
+++ app/controllers/bet_controller.rb
@@ -2,7 +2,9 @@ class BetController < ApplicationController
 	
 	def index
 		@results = Result.all.order('created_at DESC')
-		@bets = Bet.all
+		@bets = Bet.all.order('created_at DESC')
+		@points_graph = @results.sort_by(&:date).map{|x| [x.date, x.running_points_profit]}
+		@profit_graph = @results.sort_by(&:date).map{|x| [x.date, x.running_profit]}
 	end
 
 	
