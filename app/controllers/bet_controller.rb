e1f502b3ca740b8bcdae3782449e2b352361e0a0
Tom Hughes <thomas.peter.hughes@gmail.com>
2017-07-01 13:32:19 +0930

    heaps o changes

--- app/controllers/bet_controller.rb
+++ app/controllers/bet_controller.rb
@@ -2,10 +2,16 @@ class BetController < ApplicationController
 	
 	def index
 		@results = Result.all.order('created_at DESC')
-		@bets = Bet.all.order('created_at DESC')
+		@all_bets = Bet.all
+		@bets = Bet.where("created_at >= ?", Time.zone.yesterday).order('created_at DESC')
+		@points_graph = @results.sort_by(&:date).map{|x| [x.date, x.running_points_profit]}
+	end
+
+	def personal
+		@results = Result.all.order('created_at DESC')
+		@bets = Bet.where("created_at >= ?", Time.zone.yesterday).order('created_at DESC')
 		@points_graph = @results.sort_by(&:date).map{|x| [x.date, x.running_points_profit]}
 		@profit_graph = @results.sort_by(&:date).map{|x| [x.date, x.running_profit]}
 	end
-
 	
 end
