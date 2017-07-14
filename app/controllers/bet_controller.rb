7f1562feea919bcf02304eb6175e8618c5d892db
Tom Hughes <thomas.peter.hughes@gmail.com>
2017-07-14 19:48:43 +0700

    pls

--- app/controllers/bet_controller.rb
+++ app/controllers/bet_controller.rb
@@ -8,7 +8,7 @@ class BetController < ApplicationController
 		@premium = Portfolio.first
 		@results = Result.all.order('created_at DESC')
 		@pending_bets = Bet.pending_bets
-		@bets = Bet.where("created_at >= ?", Time.zone.yesterday).order('created_at ASC')
+		@bets = Bet.where("DATE(created_at) = ?", Date.parse(Time.new.gmtime.to_s)-1)
 		@profit_graph_multi = @results.where(portfolio_id: 1).sort_by(&:date).map{|x| [x.date, x.running_profit]}
 		@profit_graph_surefire = @results.where(portfolio_id: 2).sort_by(&:date).map{|x| [x.date, x.running_profit]}
 	end
