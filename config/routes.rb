5eaa36ffd8f8553fab8864102e04b01ce81edfb7
Tom Hughes <thomas.peter.hughes@gmail.com>
2017-09-09 08:54:22 +1000

    moved accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => "bet#index"
+  root :to => "bet#personal"
   match "/botlite" => 'bet#personal', via: [:get]
   match "/pe2deorfd" => 'bet#place', via: [:get]
 end
